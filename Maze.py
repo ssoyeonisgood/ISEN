59d53f29bd7c63db46441900a3f58cdc9a982998
Soyeon Kim <90024856@study.curtincollege.edu.au>
2022-03-21 21:44:59 +0800

    change the messages(4-5).

--- Maze.py
+++ Maze.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
             elif grid[currentRow][currentCol] == END:
                 done = True
                 solved = True
-                print("MESSAGE 4") # Solved.
+                print("SOLVED!") # Solved.
                 
             else:
                 pass # Do nothing
@@ -85,6 +85,6 @@ if __name__ == "__main__":
 
 
     if not solved:
-        print("MESSAGE 5") # Did not reach the end.
+        print("You have failed to escape. Future archeologists gaze upon your remains in bafflement") # Did not reach the end.
         Viewer.view(grid)
 
