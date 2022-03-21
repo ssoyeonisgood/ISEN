576c68f5ce96833d9184ef28ba5945fde3553c1e
Soyeon Kim <90024856@study.curtincollege.edu.au>
2022-03-21 22:17:33 +0800

    change words

--- Maze.py
+++ Maze.py
@@ -30,8 +30,8 @@ if __name__ == "__main__":
         + " (for north, east, south and west).")
     solution = input("Your solution: ")
 
-    currentRow = 1
-    currentCol = 0
+    Row = 1
+    Col = 0
     done = False
     solved = False
     charIndex = 0
@@ -40,39 +40,39 @@ if __name__ == "__main__":
     while not done and charIndex < solutionLength:
         
         direction = solution[charIndex]
-        print("Location: (" + str(currentRow) + ", " + str(currentCol) 
+        print("Location: (" + str(Row) + ", " + str(Col) 
             + "), next direction: '" + direction + "'")
         
         if direction == NORTH:
-            currentRow -= 1
+            Row -= 1
             
         elif direction == EAST:
-            currentCol += 1
+            Col += 1
                 
         elif direction == SOUTH:
-            currentRow += 1
+            Row += 1
                 
         elif direction == WEST:
-            currentCol -= 1
+            Col -= 1
         
         else:
             print("You have no idea where you're going.") # Invalid direction.
         
-        if (currentRow < 0 or currentCol < 0 
-                        or currentRow >= len(grid) 
-                        or currentCol >= len(grid[currentRow])):
+        if (Row < 0 or Col < 0 
+                        or Row >= len(grid) 
+                        or Col >= len(grid[Row])):
             done = True
             print("You fail into the chasm of doom") # Out of bounds.
             
         else:
-            if grid[currentRow][currentCol] == EMPTY:
-                grid[currentRow][currentCol] = VISITED
+            if grid[Row][Col] == EMPTY:
+                grid[Row][Col] = VISITED
                 
-            elif grid[currentRow][currentCol] == WALL:
+            elif grid[Row][Col] == WALL:
                 done = True
                 print("You stumble blindly into a solid concrete wall") # Hit wall.
 
-            elif grid[currentRow][currentCol] == END:
+            elif grid[Row][Col] == END:
                 done = True
                 solved = True
                 print("SOLVED!") # Solved.
