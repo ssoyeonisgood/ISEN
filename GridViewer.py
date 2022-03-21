50531b9f898016ac5b6c2e06e3af72817e59ab46
Soyeon Kim <90024856@study.curtincollege.edu.au>
2022-03-21 20:57:25 +0800

    remove the viewing code.

---
+++ GridViewer.py
@@ -0,0 +1,26 @@
+import Maze
+
+def view(grid)
+     for i in range(len(grid)):
+        for j in range(len(grid[i])):
+
+            if grid[i][j] == Maze.EMPTY:
+                print("  ", end = "")
+
+            elif grid[i][j] == Mazw.WALL:
+                print("##", end = "")
+
+            elif grid[i][j] == Maze.START:
+                print("^^", end = "")
+
+            elif grid[i][j] == Maze.END:
+                print("$$", end = "")
+
+            elif grid[i][j] == Maze.VISITED:
+                print("..", end = "")
+
+            else:
+                raise AssertionError
+
+        print()
+
--- Maze.py
+++ Maze.py
@@ -1,3 +1,5 @@
+import GridViewer
+
 EMPTY = 0
 WALL = 1
 START = 2
@@ -21,29 +23,7 @@ if __name__ == "__main__":
         [ WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL, WALL],
     ]
                     
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            
-            if grid[i][j] == EMPTY:
-                print("  ", end = "")
-                    
-            elif grid[i][j] == WALL:
-                print("##", end = "")
-                    
-            elif grid[i][j] == START:
-                print("^^", end = "")
-                    
-            elif grid[i][j] == END:
-                print("$$", end = "")
-                    
-            elif grid[i][j] == VISITED:
-                print("..", end = "")
-                    
-            else:
-                raise AssertionError
-            
-        print()
-
+    GridViewer.view(grid)
 
     print("Find a solution to get from ^^ to $$, using the characters " 
         + "'" + NORTH + "', '" + EAST + "', '" + SOUTH + "' and '" + WEST + "'"
@@ -106,27 +86,5 @@ if __name__ == "__main__":
 
     if not solved:
         print("MESSAGE 5") # Did not reach the end.
+        GridViewer.view(grid)
 
-
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            
-            if grid[i][j] == EMPTY:
-                print("  ", end = "")
-                    
-            elif grid[i][j] == WALL:
-                print("##", end = "")
-                    
-            elif grid[i][j] == START:
-                print("^^", end = "")
-                    
-            elif grid[i][j] == END:
-                print("$$", end = "")
-                    
-            elif grid[i][j] == VISITED:
-                print("..", end = "")
-                    
-            else:
-                raise AssertionError
-            
-        print()
